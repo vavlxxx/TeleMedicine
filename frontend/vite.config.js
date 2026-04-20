a2efb93a6e3fd829944bad63d198dccec0c158a4
vavlxxx <vavlxxx@vavlxxx.com>
2026-04-20 14:17:14 +0000

    fixed nginx configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [react(), tailwindcss()],
     server: {
+      allowedHosts: ["virtualmedic.ru"],
       host: '0.0.0.0',
       port: 5173,
       proxy: {
